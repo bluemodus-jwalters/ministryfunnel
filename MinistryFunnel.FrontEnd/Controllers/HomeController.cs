dccfc6633dbc28af7a6e8e399b5fd300661f78d2
Jordan Walters <jwalters@bluemodus.com>
2021-07-25 06:40:26 -0400

    /home/signin working with Azure AD to authenticate

--- MinistryFunnel.FrontEnd/Controllers/HomeController.cs
+++ MinistryFunnel.FrontEnd/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using MinistryFunnel.FrontEnd.Helpers;
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using MinistryFunnel.FrontEnd.Helpers;
 using MinistryFunnel.FrontEnd.Models;
 using Newtonsoft.Json;
 using System;
@@ -31,7 +33,33 @@ namespace MinistryFunnel.FrontEnd.Controllers
             return View();
         }
 
-       
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+            else
+            {
+                var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+                //You get the user’s first and last name below:
+                ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+
+                // The 'preferred_username' claim can be used for showing the username
+                ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+
+                // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+                ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+                // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+                ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+            }
+        }
+
+
 
         private string CompileUrl(string action)
         {
